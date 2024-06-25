d39dc677351e67c5e7e73ff7a91458ed4b2ad678
anupam1411 <sagaranupam2@gmail.com>
2024-06-25 20:47:11 +0530

    job form updated

--- components/JobForm.jsx
+++ components/JobForm.jsx
@@ -1,218 +1,224 @@
 import React from "react";
 
-function JobFrom({ formData, onChange, onSubmit }) {
+function JobForm({ formData, onChange, onSubmit }) {
   return (
-    <div>
-      <form onSubmit={onSubmit} className="max-w-lg mx-auto">
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Job Title:
-          </label>
-          <input
-            type="text"
-            name="jobTitle"
-            value={formData.jobTitle}
-            onChange={onChange}
-            required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
+    <div className="max-w-7xl mx-auto p-4">
+      <form
+        onSubmit={onSubmit}
+        className="grid grid-cols-1 lg:grid-cols-3 gap-4"
+      >
+        <div className="bg-blue-200 p-4 rounded">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Job Title:
+            </label>
+            <input
+              type="text"
+              name="jobTitle"
+              value={formData.jobTitle}
+              onChange={onChange}
+              required
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Description:
+            </label>
+            <textarea
+              name="description"
+              value={formData.description}
+              onChange={onChange}
+              required
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Location:
+            </label>
+            <input
+              type="text"
+              name="location"
+              value={formData.location}
+              onChange={onChange}
+              required
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Remote Option:
+            </label>
+            <select
+              name="remoteOption"
+              value={formData.remoteOption}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            >
+              <option value="no">select</option>
+              <option value="Yes">Yes</option>
+              <option value="No">No</option>
+            </select>
+            <span className="text-sm">
+              {formData.remoteOption}, remote option available
+            </span>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Skills Required:
+            </label>
+            <input
+              type="text"
+              name="skillsRequired"
+              value={formData.skillsRequired}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Salary Range:
+            </label>
+            <input
+              type="text"
+              name="salaryRange"
+              value={formData.salaryRange}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Description:
-          </label>
-          <textarea
-            name="description"
-            value={formData.description}
-            onChange={onChange}
-            required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
+
+        <div className="bg-blue-200 p-4 rounded">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Job Type:
+            </label>
+            <select
+              name="jobType"
+              value={formData.jobType}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            >
+              <option value="fulltime">Full-time</option>
+              <option value="parttime">Part-time</option>
+              <option value="internship">Internship</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Experience:
+            </label>
+            <input
+              type="text"
+              name="experience"
+              value={formData.experience}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Education:
+            </label>
+            <input
+              type="text"
+              name="education"
+              value={formData.education}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              CGPA or Percentage Scored:
+            </label>
+            <input
+              type="text"
+              name="cgpaOrPercentage"
+              value={formData.cgpaOrPercentage}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Last Date to Apply:
+            </label>
+            <input
+              type="date"
+              name="lastDateToApply"
+              value={formData.lastDateToApply}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Contact Email:
+            </label>
+            <input
+              type="email"
+              name="contactEmail"
+              value={formData.contactEmail}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Contact Phone:
+            </label>
+            <input
+              type="tel"
+              name="contactPhone"
+              value={formData.contactPhone}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Location:
-          </label>
-          <input
-            type="text"
-            name="location"
-            value={formData.location}
-            onChange={onChange}
-            required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
+
+        <div className="bg-blue-200 p-4 rounded">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Company Name:
+            </label>
+            <input
+              type="text"
+              name="companyName"
+              value={formData.companyName}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Company Website:
+            </label>
+            <input
+              type="url"
+              name="companyWebsite"
+              value={formData.companyWebsite}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Company Address:
+            </label>
+            <textarea
+              name="companyAddress"
+              value={formData.companyAddress}
+              onChange={onChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Remote Option:
-          </label>
-          <select
-            name="remoteOption"
-            value={formData.remoteOption}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          >
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
-          </select>
-          {/* <input
-            type="checkbox"
-            name="remoteOption"
-            checked={formData.remoteOption}
-            onChange={onChange}
-            className="mr-2 leading-tight"
-          /> */}
-          <span className="text-sm">
-            {formData.remoteOption}, remote option available
-          </span>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Job Type:
-          </label>
-          <select
-            name="jobType"
-            value={formData.jobType}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          >
-            <option value="fulltime">Full-time</option>
-            <option value="parttime">Part-time</option>
-            <option value="internship">Internship</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Salary Range:
-          </label>
-          <input
-            type="text"
-            name="salaryRange"
-            value={formData.salaryRange}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Skills Required:
-          </label>
-          <input
-            type="text"
-            name="skillsRequired"
-            value={formData.skillsRequired}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Experience:
-          </label>
-          <input
-            type="text"
-            name="experience"
-            value={formData.experience}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Education:
-          </label>
-          <input
-            type="text"
-            name="education"
-            value={formData.education}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            CGPA or Percentage Scored:
-          </label>
-          <input
-            type="text"
-            name="cgpaOrPercentage"
-            value={formData.cgpaOrPercentage}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Last Date to Apply:
-          </label>
-          <input
-            type="date"
-            name="lastDateToApply"
-            value={formData.lastDateToApply}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Contact Email:
-          </label>
-          <input
-            type="email"
-            name="contactEmail"
-            value={formData.contactEmail}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Contact Phone:
-          </label>
-          <input
-            type="tel"
-            name="contactPhone"
-            value={formData.contactPhone}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Company Name:
-          </label>
-          <input
-            type="text"
-            name="companyName"
-            value={formData.companyName}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Company Website:
-          </label>
-          <input
-            type="url"
-            name="companyWebsite"
-            value={formData.companyWebsite}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Company Address:
-          </label>
-          <textarea
-            name="companyAddress"
-            value={formData.companyAddress}
-            onChange={onChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="flex items-center justify-between">
+        <div className="lg:col-span-3 flex items-center justify-center mt-4">
           <button
             type="submit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -225,4 +231,4 @@ function JobFrom({ formData, onChange, onSubmit }) {
   );
 }
 
-export default JobFrom;
+export default JobForm;
